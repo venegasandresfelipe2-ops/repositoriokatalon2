1cdb5d5524ca1339de5ddf6f1df3922a8ac62e7a
Andres220444 <andres_venegas@soy.sena.edu.co>
2026-08-01 16:34:03 -0500

    Compañero modifica Caso 3

--- Scripts/CASO_DE_PRUEBA_3/Script1785466156680.groovy
+++ Scripts/CASO_DE_PRUEBA_3/Script1785466156680.groovy
@@ -17,3 +17,4 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+// Cambio realizado por el compañero
